0bad1b575aa4ac3bae95465d719cab120c2a707b
Beatriz <132356935+abeatrizoliveira@users.noreply.github.com>
2025-11-08 07:38:36 -0300

    Adiciona constraints unique em continente, pais, e cidade
    Adiciona a restrição "unique" nas tabelas: continente, pais e cidade.

--- bd/database.sql
+++ bd/database.sql
@@ -3,13 +3,13 @@ use bd_mundo;
 
 create table continente(
     id_continente int primary key auto_increment,
-    nm_continente varchar(50) not null
+    nm_continente varchar(50) unique not null
 );
 
 create table pais(
     id_pais int primary key auto_increment,
-    nm_pais varchar(50) not null,
-    cd_pais varchar(3) not null,
+    nm_pais varchar(50) unique not null,
+    cd_pais varchar(3) unique not null,
     qtd_populacao bigint not null,
     nm_idioma varchar(50) not null,
     cd_continente int not null,
@@ -18,7 +18,7 @@ create table pais(
 
 create table cidade(
     id_cidade int primary key auto_increment,
-    nm_cidade varchar(50) not null,
+    nm_cidade varchar(50) unique not null,
     qtd_populacao bigint not null,
     cd_pais int not null,
     foreign key (cd_pais) references pais(id_pais)
